1d7d1511163f0a3eb1366d1c73cdaf8a877ce337
AJ Networks <jepil.choi@ajway.kr>
2017-08-28 17:00:44 +0900

    [MODIFY] dns to url

--- modules/proxy_config.js
+++ modules/proxy_config.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var dns = require('dns')
 
 module.exports = function(){
     this.master_url = ''
@@ -6,25 +7,32 @@ module.exports = function(){
     return {
         set_master_url : () => {
             return new Promise((resolve, reject) => {
-                request("mr-redis.marathon.mesos:5656/v1/STATUS", (error, response, body) => {
+                dns.lookup("mr-redis.marathon.mesos", (error, address, family) => {
                     if (error != undefined) {
                         reject(error)
                         return
                     }
 
-                    let config_json = JSON.parse(body)
-
-                    if (config_json.length == 0) {
-                        if (config_json[0].Status == "RUNNING") {
+                    request(`${address}:5656/v1/STATUS`, (error, response, body) => {
+                        if (error != undefined) {
+                            reject(error)
                             return
                         }
 
-                        reject("REDIS Cluster Error!")
-                    }
+                        let config_json = JSON.parse(body)
 
-                    let master_config = config_json[0].Master
-                    this.master_url = `${master_config.IP}:${master_config.Port}`
-                    resolve(this.master_url)
+                        if (config_json.length == 0) {
+                            if (config_json[0].Status == "RUNNING") {
+                                return
+                            }
+
+                            reject("REDIS Cluster Error!")
+                        }
+
+                        let master_config = config_json[0].Master
+                        this.master_url = `http://${master_config.IP}:${master_config.Port}`
+                        resolve(this.master_url)
+                    })
                 })
             })
         },
